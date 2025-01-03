c4fc826fd895251c635b18ed8cda671e1f4306a9
sofimes <sofanitmesfin19@gmail.com>
2025-01-03 22:32:16 +0300

    `Update Footer component with minor styling changes and additional contact information`

--- client/src/common/Footer.jsx
+++ client/src/common/Footer.jsx
@@ -7,7 +7,7 @@ import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-24 mt-12">
+    <footer className="bg-gray-800 text-white pb-20 pt-6 mt-12">
       <div className="container mx-auto flex flex-wrap justify-between items-center px-4 md:px-20">
         {/* Logo and Social Icons */}
         <div className="w-full md:w-1/3 mb-8 md:mb-0 text-center md:text-left">
@@ -18,14 +18,24 @@ const Footer = () => {
             </h1>
           </Link>
           <div className="flex justify-center md:justify-start space-x-4">
-            <BsFacebook
-              size="30"
-              className="cursor-pointer hover:text-blue-500"
-            />
-            <AiFillInstagram
-              size="35"
-              className="cursor-pointer hover:text-pink-500"
-            />
+            <Link
+              to={
+                "https://web.facebook.com/profile.php?id=61567874451604&sk=about"
+              }
+            >
+              <BsFacebook
+                size="30"
+                className="cursor-pointer hover:text-blue-500"
+              />
+            </Link>
+
+            <Link to={"https://www.instagram.com/sofanit1927/"}>
+              <AiFillInstagram
+                size="35"
+                className="cursor-pointer hover:text-pink-500"
+              />
+            </Link>
+
             <FaYoutube
               size="35"
               className="cursor-pointer hover:text-red-500"
@@ -48,13 +58,19 @@ const Footer = () => {
 
         {/* Contact Info */}
         <div className="w-full md:w-1/3 text-center md:text-left">
-          <h3 className="text-lg font-semibold mb-4">Contact Info</h3>
+          <h3 className="text-lg font-semibold mb-4">Contact Us</h3>
           <ul>
             <li className="text-gray-400 text-sm hover:text-white mb-2">
               sofanitmesfin19@gmail.com
             </li>
-            <li className="text-gray-400 text-sm hover:text-white">
+            <li className="text-gray-400 text-sm hover:text-white mb-2">
               +251-991-50-00-27
+            </li>
+            <li className="text-gray-400 text-sm hover:text-white mb-2">
+              4 killo
+            </li>
+            <li className="text-gray-400 text-sm hover:text-white mb-2">
+              Addis Ababa, Ethiopia.
             </li>
           </ul>
         </div>
